ca575af36a55640eb50b2c619eca4477b34c4dc0
Zigm4s <Zigm4s@gmail.com>
2014-05-18 21:27:13 +0200

    Update of files

---
+++ RussianRouletteClient/RussianRouletteClient/GameForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace RussianRouletteClient
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_Game = new System.Windows.Forms.GroupBox();
+            this.btn_Spin = new System.Windows.Forms.Button();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.btn_Rematch = new System.Windows.Forms.Button();
+            this.btn_Fire = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_ChatBox = new System.Windows.Forms.ListBox();
+            this.btn_SendMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gb_Game
+            // 
+            this.gb_Game.Location = new System.Drawing.Point(12, 41);
+            this.gb_Game.Name = "gb_Game";
+            this.gb_Game.Size = new System.Drawing.Size(580, 311);
+            this.gb_Game.TabIndex = 0;
+            this.gb_Game.TabStop = false;
+            this.gb_Game.Text = "Game ";
+            // 
+            // btn_Spin
+            // 
+            this.btn_Spin.Location = new System.Drawing.Point(12, 12);
+            this.btn_Spin.Name = "btn_Spin";
+            this.btn_Spin.Size = new System.Drawing.Size(75, 23);
+            this.btn_Spin.TabIndex = 1;
+            this.btn_Spin.Text = "Spin";
+            this.btn_Spin.UseVisualStyleBackColor = true;
+            this.btn_Spin.Click += new System.EventHandler(this.btn_Spin_Click);
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Location = new System.Drawing.Point(169, 12);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(75, 23);
+            this.btn_Play.TabIndex = 2;
+            this.btn_Play.Text = "Play";
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // btn_Rematch
+            // 
+            this.btn_Rematch.Location = new System.Drawing.Point(336, 12);
+            this.btn_Rematch.Name = "btn_Rematch";
+            this.btn_Rematch.Size = new System.Drawing.Size(75, 23);
+            this.btn_Rematch.TabIndex = 3;
+            this.btn_Rematch.Text = "Rematch";
+            this.btn_Rematch.UseVisualStyleBackColor = true;
+            this.btn_Rematch.Click += new System.EventHandler(this.btn_Rematch_Click);
+            // 
+            // btn_Fire
+            // 
+            this.btn_Fire.Location = new System.Drawing.Point(517, 12);
+            this.btn_Fire.Name = "btn_Fire";
+            this.btn_Fire.Size = new System.Drawing.Size(75, 23);
+            this.btn_Fire.TabIndex = 4;
+            this.btn_Fire.Text = "Fire";
+            this.btn_Fire.UseVisualStyleBackColor = true;
+            this.btn_Fire.Click += new System.EventHandler(this.btn_Fire_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 487);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lb_ChatBox
+            // 
+            this.lb_ChatBox.FormattingEnabled = true;
+            this.lb_ChatBox.Location = new System.Drawing.Point(12, 358);
+            this.lb_ChatBox.Name = "lb_ChatBox";
+            this.lb_ChatBox.Size = new System.Drawing.Size(459, 121);
+            this.lb_ChatBox.TabIndex = 6;
+            // 
+            // btn_SendMessage
+            // 
+            this.btn_SendMessage.Location = new System.Drawing.Point(477, 358);
+            this.btn_SendMessage.Name = "btn_SendMessage";
+            this.btn_SendMessage.Size = new System.Drawing.Size(115, 149);
+            this.btn_SendMessage.TabIndex = 7;
+            this.btn_SendMessage.Text = "Send Message";
+            this.btn_SendMessage.UseVisualStyleBackColor = true;
+            this.btn_SendMessage.Click += new System.EventHandler(this.btn_SendMessage_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 519);
+            this.Controls.Add(this.btn_SendMessage);
+            this.Controls.Add(this.lb_ChatBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Fire);
+            this.Controls.Add(this.btn_Rematch);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.btn_Spin);
+            this.Controls.Add(this.gb_Game);
+            this.Name = "GameForm";
+            this.Text = "GameForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_Game;
+        private System.Windows.Forms.Button btn_Spin;
+        private System.Windows.Forms.Button btn_Play;
+        private System.Windows.Forms.Button btn_Rematch;
+        private System.Windows.Forms.Button btn_Fire;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox lb_ChatBox;
+        private System.Windows.Forms.Button btn_SendMessage;
+    }
+}
---
+++ RussianRouletteClient/RussianRouletteClient/GameForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RussianRouletteClient.RussianRouletteService;
+using System.ServiceModel;
+
+namespace RussianRouletteClient
+{
+    public partial class GameForm : Form, IGameCallback
+    {
+        #region IGame Callbacks
+        public void PlayerSentMessage()
+        {
+
+        }
+
+        public void PlayerDisconnected()
+        {
+
+        }
+
+
+        public void PlayerReady()
+        {
+
+        }
+
+
+        public void PlayerLost()
+        {
+
+        }
+        #endregion
+
+        private GameClient _gameClient = null;
+        private InstanceContext _instance = null;
+
+        public GameForm()
+        {
+            _gameClient = new GameClient(new InstanceContext(this));
+            InitializeComponent();
+            _gameClient.Open();
+        }
+
+        private void btn_Spin_Click(object sender, EventArgs e)
+        {
+            _gameClient.SpingCylinder();
+        }
+
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            _gameClient.Play();
+        }
+
+        private void btn_Rematch_Click(object sender, EventArgs e)
+        {
+            _gameClient.Rematch();
+        }
+
+        private void btn_Fire_Click(object sender, EventArgs e)
+        {
+            _gameClient.Shoot(new User() { });
+        }
+
+        private void btn_SendMessage_Click(object sender, EventArgs e)
+        {
+            _gameClient.SendMessage(new UMessage() { });
+        }
+
+
+    }
+}
